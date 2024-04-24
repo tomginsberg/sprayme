55e926cc375eb2406309436237ebe0eac86ca35b
Tom Ginsberg <tom@beit.tech>
2024-04-24 13:22:26 -0400

    fixed endpoint

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -48,7 +48,7 @@ onMounted(async () => {
     climbData.value.name = 'New Route';
     lock.value = false;
   } else {
-    const response = await axios.get(`${endpoint}/climb/${climbID}`);
+    const response = await axios.get(`${endpoint}/get_route/${climbID}`);
     if (response.data.valueOf() === 404) {
       await router.push('/');
     }
